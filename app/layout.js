edb04a8c087597c843731282ba83be5e112ff976
Ahmed_khalil <ahgadoora2002@gmail.com>
2025-11-05 14:45:23 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,9 +7,14 @@ const cairo = Cairo({
   weight: ["400", "700"],
 });
 
+export const metadata = {
+  title: "موقعي التعليمي",
+  description: "موقع تدريب ودورات أونلاين",
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="ar" dir="rtl">
+    <html lang="ar">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
