7b17d7444b1a504202d6e1f525ff2f9a887f8310
Ahmed_khalil <ahgadoora2002@gmail.com>
2025-11-09 21:55:02 +0200

    Update page.jsx

--- app/faq-page/page.jsx
+++ app/faq-page/page.jsx
@@ -74,11 +74,12 @@ export default function FAQPage() {
         >
           <FaWhatsapp /> تواصل عبر واتساب
         </Link>
-      </footer>
-    </div>
 {/* الفاصل السفلي */}
       <div className="absolute bottom-4 left-0 right-0 text-center text-gray-50 text-sm">
         © {new Date().getFullYear()} مركز المصباح للتدريب المهني — جميع الحقوق محفوظة.
       </div>
+      </footer>
+    </div>
+
   );
 }
