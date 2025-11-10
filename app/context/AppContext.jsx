8a651bf55e2f0523b5f477902ce2f31eb6e0b9d1
Ahmed_khalil <ahgadoora2002@gmail.com>
2025-11-10 17:18:03 +0200

    Update AppContext.jsx

--- app/context/AppContext.jsx
+++ app/context/AppContext.jsx
@@ -16,6 +16,7 @@ const DICT = {
     campaigns_title: "حملاتنا الإعلانية",
     add_course: "إضافة دورة",
     courses: "الدورات",
+    Human Resources Manager:" مدير الموارد البشرية", 
   },
   EN: {
     login: "Login",
@@ -31,6 +32,7 @@ const DICT = {
     campaigns_title: "Our Campaigns",
     add_course: "Add Course",
     courses: "Courses",
+    Human Resources Manager:"Human Resources Manager", 
   },
 };
 
